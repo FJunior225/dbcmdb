7ff6ea61179b576b3588611be0503dd771351262
Brian Bier <briansbier@gmail.com>
2016-04-18 15:21:16 -0400

    Create validations for models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ActiveRecord::Base
   has_many :films, through: :categories_films
+  validates :name, presence: true
 end
--- app/models/film.rb
+++ app/models/film.rb
@@ -1,5 +1,6 @@
 class Film < ActiveRecord::Base
-has_many :Categories, through: :categories_films
+has_many :Categories, through: :categories_films 
+validates :title,:description, presence: true
 # has_many :ratings as: :ratable
 # has_many :reviews 
 # has_many :comments as: :commentable
