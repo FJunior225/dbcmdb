319f82951bc05df36493fa37530b7d47354d50e5
FJ Collins Jr <fcollins@live.com>
2016-04-18 15:18:05 -0400

    Add reviews controller

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,28 @@
+class ReviewsController < ApplicationController
+
+  def new
+    @film = Film.find(params[:id])
+  end
+
+  def create
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to action: 'show', controller:'toilets' , id:params[:review][:toilet_id].to_i
+    else
+      flash.now[:danger] = 'You must enter all fields and be logged in to post a review'
+      render 'new'
+    end
+  end
+
+  def show
+    @reviews = Review.paginate(:page => params[:page])
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:review_title, :review_content, :recommend, :user_id, :film_id)
+  end
+
+
+end
