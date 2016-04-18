4f3f2092bb96a136f1a4792a31f4403e64610c47
drumguy16 <ripley.brett@gmail.com>
2016-04-18 15:44:34 -0400

    Fix index on model/migraion for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ActiveRecord::Base
 	has_many :ratings
 
 	validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
-	validates :username, presence: true, uniqueness: true, index: true
+	validates :username, presence: true, uniqueness: true
 
 end
