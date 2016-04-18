65112c450665c0e0b270e21657550c1df226c81e
Brian Bier <briansbier@gmail.com>
2016-04-18 15:40:00 -0400

    Update films table to include picture column

--- db/schema.rb
+++ db/schema.rb
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20160418190734) do
   create_table "films", force: :cascade do |t|
     t.string   "title",       null: false
     t.string   "description", null: false
+    t.string   "picture"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
