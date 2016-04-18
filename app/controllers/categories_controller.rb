afb50e5b58e8546b62797454fd0b265c07398048
Brian Bier <briansbier@gmail.com>
2016-04-18 17:45:44 -0400

    Include link form for category page and include show route

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,12 @@
 class CategoriesController < ApplicationController
 
 def index
+  @categories = Category.take(6)
+end
 
+def show
+  binding.pry
+  @category =  Category.find(params[:id])
 end
 
 end
