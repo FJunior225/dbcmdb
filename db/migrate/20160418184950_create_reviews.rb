c595e5c02d36374dc3f8afe13f83fc36c8b3a9da
FJ Collins Jr <fcollins@live.com>
2016-04-18 14:56:52 -0400

    Add review model and table with associations

---
+++ db/migrate/20160418184950_create_reviews.rb
@@ -0,0 +1,13 @@
+class CreateReviews < ActiveRecord::Migration
+  def change
+    create_table :reviews do |t|
+      t.string :review_title, null: false
+      t.textarea :review_content, null: false
+      t.boolean :recommend, null: false
+      t.references :user, null: false, index: true
+      t.references :film, null: false, index: true
+
+      t.timestamps null: false
+    end
+  end
+end
