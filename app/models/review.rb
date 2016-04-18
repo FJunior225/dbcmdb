d79760a1a148b75cff442390f5261cf89f1f6ea2
FJ Collins Jr <fcollins@live.com>
2016-04-18 15:08:19 -0400

    adjust review model to reference polymorphic comments

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,8 @@
 class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :film
-  has_many :comments, as :commentable
-  has_many :ratings, as :ratable
+  has_many :comments, as: :commentable
+  has_many :ratings, as: :ratable
 
   validates :review_title, :review_content, :user_id, :film_id, { presence: true }
 
--- db/migrate/20160418184950_create_reviews.rb
+++ db/migrate/20160418184950_create_reviews.rb
@@ -2,7 +2,7 @@ class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
       t.string :review_title, null: false
-      t.textarea :review_content, null: false
+      t.text :review_content, null: false
       t.boolean :recommend, null: false
       t.references :user, null: false, index: true
       t.references :film, null: false, index: true
