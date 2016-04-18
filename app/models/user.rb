d7680f047c6633ab46cd4a92e0ff9871eb943f1b
drumguy16 <ripley.brett@gmail.com>
2016-04-18 19:14:02 -0400

    Add twitter and facebook omniauth gems. Fixed migration conflicts with user and devise migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,11 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
 	has_secure_password
 
 	has_many :reviews
 	has_many :comments
 	has_many :ratings
 
-	validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+	# validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 	validates :username, presence: true, uniqueness: true
 
-end
+end 
