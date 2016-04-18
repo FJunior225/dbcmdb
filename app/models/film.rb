efde579d798157ed6f98964b09b91b92851eb3cd
Brian Bier <briansbier@gmail.com>
2016-04-18 15:27:48 -0400

    Fix association problem with the many to many

--- app/models/film.rb
+++ app/models/film.rb
@@ -1,7 +1,10 @@
 class Film < ActiveRecord::Base
-has_many :Categories, through: :categories_films 
+has_many :categories_films 
+has_many :categories, through: :categories_films 
+has_many :reviews 
+
+has_many :ratings, as: :ratable
+has_many :comments, as: :commentable
+
 validates :title,:description, presence: true
-# has_many :ratings as: :ratable
-# has_many :reviews 
-# has_many :comments as: :commentable
 end
