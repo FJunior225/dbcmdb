250efdf4ad088e15ea15e893a732f30388894d3c
drumguy16 <ripley.brett@gmail.com>
2016-04-19 10:31:42 -0400

    Add FB and Twitter key variables to devise.rb

--- db/migrate/20160418185404_create_users.rb
+++
@@ -1,9 +0,0 @@
-class CreateUsers < ActiveRecord::Migration
-  def change
-    create_table :users do |t|
-      t.string :username, null: false, index: true
-      t.string :password_digest, null: false
-      t.boolean :trusted, default: false
-    end
-  end
-end
