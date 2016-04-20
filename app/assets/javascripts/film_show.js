282b53f40b1bc3e4e580e22af542845448006949
drumguy16 <ripley.brett@gmail.com>
2016-04-19 20:56:37 -0400

    Make tab active classes work with jquery in film_show.js

--- app/assets/javascripts/film_show.js
+++ app/assets/javascripts/film_show.js
@@ -3,13 +3,18 @@ $(document).ready(function(){
 	$(".nav-tabs").on('click','.trusted', function(event) {
 		event.preventDefault();
 		$('.trusted-partial').show();
-		$('.recent-partial').hide();
+		$('.trusted').addClass('active');
+		$('.recent-partial').removeClass('active').hide();
+		$('.recent').removeClass('active')
 	});
 
 	$(".nav-tabs").on('click','.recent', function(event) {
 		event.preventDefault();
 		$('.recent-partial').show();
+		$('.recent').addClass('active');
 		$('.trusted-partial').hide();
+		$('.trusted').removeClass('active');
+
 	})
 
 
