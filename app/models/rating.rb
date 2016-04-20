7eb26bd45409138181858c44b3f3494fc42ee33b
FJ Collins Jr <fcollins@live.com>
2016-04-20 14:21:13 -0400

    Add unique rating - still needs to complete

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -3,6 +3,16 @@ class Rating < ActiveRecord::Base
 	belongs_to :user
 
 	validates :rating, :user_id, { presence: true }
+  validates_uniqueness_of :user, :scope => [:votable_id, :votable_type]
+  # validates_uniqueness_of :rateable_id, :rateable_type, { scope: :user_id }
 
+  # validate :unique_rate
+
+
+  # def unique_rate()
+  #   if Rating.where(user: current_user, rateable_id: self.rateable_id, rateable_type: self.rateable_type)
+  #     errors.add(:rateable_id, "Can't rate more than once!")
+  #   end
+  # end
 
 end
