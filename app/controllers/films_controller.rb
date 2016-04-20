05ecfa2070087b9d81fb2645cd38a6ea90167090
drumguy16 <ripley.brett@gmail.com>
2016-04-20 10:15:33 -0400

    took care of conflict

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -1,16 +1,16 @@
 class FilmsController < ApplicationController
 
-<<<<<<< HEAD
+
 	def index
 		@films = Film.all
 	end
-	
+
   def show
     @film = Film.find(params[:id])
     @rating = Rating.new
     @review = Review.new
     @comment = Comment.new
   end
->>>>>>> master
+
 
 end
